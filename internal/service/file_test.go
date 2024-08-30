c7c10def0b46dc45b684860620bf92d79f7fb3aa
escoutdoor <vanap387@gmail.com>
2024-08-30 18:10:13 +0300

    feat: file test created

--- internal/service/file_test.go
+++ internal/service/file_test.go
@@ -1,41 +1,64 @@
 package service
 
 import (
+	"bytes"
+	"context"
+	"mime/multipart"
 	"testing"
 
+	"github.com/brianvoe/gofakeit/v7"
+	"github.com/escoutdoor/social/internal/testutils"
 	"github.com/stretchr/testify/suite"
 	"github.com/testcontainers/testcontainers-go"
 )
 
 type fileServiceSuite struct {
 	suite.Suite
-	container      testcontainers.Container
-	minioContainer testcontainers.Container
-	svc            File
+	container testcontainers.Container
+	svc       File
 }
 
-func (st *fileServiceSuite) SetupTest() {
-	// container, db, err := testutils.NewPostgresContainer()
-	// st.Require().NoError(err, "failed to run postgres container")
-	// st.Require().NotEmpty(container, "expected to get postgres container")
-	// st.Require().NotEmpty(db, "expected to get db connection")
-	//
-	// minioContainer, s3, err := testutils.NewMinIOContainer()
-	// st.Require().NoError(err, "failed to run minio container")
-	// st.Require().NotEmpty(minioContainer, "expected to get minio container")
-	// st.Require().NotEmpty(s3, "expected to get minio connection")
-	//
-	// st.container = container
-	// st.minioContainer = minioContainer
-	// st.svc = NewFileService(s3)
+func (st *fileServiceSuite) SetupSuite() {
+	container, s3, err := testutils.NewMinIOContainer()
+	st.Require().NoError(err, "failed to run minio container")
+	st.Require().NotEmpty(container, "expected to get minio container")
+	st.Require().NotEmpty(s3, "expected to get minio connection")
+
+	st.container = container
+	st.svc = NewFileService(s3)
+}
+
+func (st *fileServiceSuite) TearDownSuite() {
+	err := st.container.Terminate(context.Background())
+	st.Require().NoError(err, "failed to terminate postgres container")
 }
 
 func (st *fileServiceSuite) TestCreate() {
-	// ctx := context.Background()
+	var (
+		body    bytes.Buffer
+		content = []byte("wassup")
+		ctx     = context.Background()
+	)
 
-	// url, err := st.svc.Create(ctx, file, hdr)
-	// st.Require().NoError(err, "failed to create file")
-	// st.Require().NotEmpty(url, "expected to get file URL")
+	mw := multipart.NewWriter(&body)
+	fw, err := mw.CreateFormFile("file", "file.jpg")
+	st.NoError(err, "failed to create form file")
+
+	n, err := fw.Write(content)
+	st.NoError(err, "failed to write content into file")
+	st.Equal(len(content), n)
+
+	err = mw.Close()
+	st.NoError(err, "failed to close writer")
+
+	hdr := &multipart.FileHeader{
+		Filename: gofakeit.BeerName(),
+		Size:     int64(len(body.Bytes())),
+	}
+
+	url, err := st.svc.Create(ctx, &body, hdr)
+	st.NoError(err, "failed to store photo into s3")
+	st.NotEmpty(url, "expected to get photo url")
 }
 
 func TestFileService(t *testing.T) {
